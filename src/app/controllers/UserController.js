e5619903ad9c0af63d2e6e9849f004a7a2d147e2
PatrickMendC <patrick.navi_igb@hotmail.com>
2019-09-05 21:04:18 -0400

    feat: add metodo update

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -10,6 +10,25 @@ class UserController {
     const user = await User.create(req.body);
     return res.json(user);
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+      if (userExists) {
+        return res.status(400).json({ error: 'User Already Exists' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password doesnot Match' });
+    }
+
+    const userUpdate = await user.update(req.body);
+    return res.json(userUpdate);
+  }
 }
 
 export default new UserController();
